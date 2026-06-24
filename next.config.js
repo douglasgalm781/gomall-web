9233f8bac348aa5c55102bdc9ad74ecc2578b2e2
passion-dev-group <fiona965ds@gmail.com>
2026-06-24 13:21:30 -0300

    test

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:4000";
 
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   async rewrites() {
     return [
       { source: "/api/:path*", destination: `${BACKEND_URL}/api/:path*` },
